4e554e198ae54cd8c035e0520a9e10ea9c4db172
Caio F <caiofsousa27@hotmail.com>
2022-08-29 11:53:40 -0300

    refactor: getOne

--- backend/src/main/routes/products-routes.ts
+++ backend/src/main/routes/products-routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 import { makeProductDeleteController } from "../factories/product-delete";
 import { makeProductGetController } from "../factories/product-get";
+import { makeProductGetOneController } from "../factories/product-getOne";
 import { makeProductPostController } from "../factories/product-post";
 import { makeProductPutController } from "../factories/product-put";
 
 export default (router: Router): void => {
   router.post("/products",adaptRoute(makeProductPostController()));
   router.get("/products",adaptRoute(makeProductGetController()));
-  router.get("/products/:id");
+  router.get("/products/:id",adaptRoute(makeProductGetOneController()));
   router.delete("/products",adaptRoute(makeProductDeleteController()));
   router.put("/products",adaptRoute(makeProductPutController()));
 };
